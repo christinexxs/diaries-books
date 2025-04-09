c99be1ce967041b47e5ce61760b3114c3723660f
Vick <vitoria.cms.veleiros@gmail.com>
2025-04-09 16:06:06 -0300

    09/04/2025

--- js/script.js
+++ js/script.js
@@ -1,56 +1,59 @@
-
 (function () {
 
-
-    "use strict";
+  "use strict";
   /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
 
+  /* Função de seleção */
 
-  /**
-   * Função de seleção
-   */
- 
   const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+      el = el.trim()
+      if (all) {
+          return [...document.querySelectorAll(el)]
+      } else {
+          return document.querySelector(el)
+      }
   }
 
-
   /**
-   * Easy event listener function
+   * Easy event listener function 
    * Função de Adição de Eventos (on)
    * Função espera que um evento ocorra e depois responde a ele  */
- 
- 
+
+
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
+      let selectEl = select(el, all)
+      if (selectEl) {
+          if (all) {
+              selectEl.forEach(e => e.addEventListener(type, listener))
+          } else {
+              selectEl.addEventListener(type, listener)
+          }
       }
-    }
   }
-/* ativar menu mobile */
+
+  // Ativar Botao Menu no modo Mobile
+
+  on('click', '.mobile-nav-toggle', function () {
+      select('body').classList.toggle('mobile-nav-active')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+  })
+
+  // Efeito Digitação
+  const typed = select('.typed')
+
+  if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items')
+      typed_strings = typed_strings.split(',')
+
+      new Typed('.typed', {
+          strings: typed_strings,
+
+      });
+
+  }
 
 
-on('click', '.mobile-nav-toggle', function() {
-    select('body').classList.toggle('mobile-nav-active')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-} )
 
 
 })()
-
-
-
-
-
-
-
