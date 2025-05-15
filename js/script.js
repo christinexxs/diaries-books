e4ff16886b91b3f324a206e73294442d99d8785a
Vick <vitoria.cms.veleiros@gmail.com>
2025-05-15 16:11:18 -0300

    att 15/05/2025

--- js/script.js
+++ js/script.js
@@ -1,97 +1,108 @@
+
 (function () {
 
-  "use strict";
-  /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
+    "use strict";
+    /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
 
-  /* Função de seleção */
+    /* Função de seleção */
 
-  const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-          return [...document.querySelectorAll(el)]
-      } else {
-          return document.querySelector(el)
-      }
-  }
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
 
-  /**
-   * Easy event listener function 
-   * Função de Adição de Eventos (on)
-   * Função espera que um evento ocorra e depois responde a ele  */
-
-  const on = (type, el, listener, all = false) => {
-      let selectEl = select(el, all)
-      if (selectEl) {
-          if (all) {
-              selectEl.forEach(e => e.addEventListener(type, listener))
-          } else {
-              selectEl.addEventListener(type, listener)
-          }
-      }
-  }
-
-  // Ativar Botao Menu no modo Mobile
-
-  on('click', '.mobile-nav-toggle', function () {
-      select('body').classList.toggle('mobile-nav-active')
-      this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
-  })
+    /**
+     * Easy event listener function 
+     * Função de Adição de Eventos (on)
+     * Função espera que um evento ocorra e depois responde a ele  */
 
 
-  // Efeito Digitação
-  const typed = select('.typed')
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
 
-  if (typed) {
-      let typed_strings = typed.getAttribute('data-typed-items')
-      typed_strings = typed_strings.split(',')
+    // Ativar Botao Menu no modo Mobile
 
-      new Typed('.typed', {
-          strings: typed_strings,
-          loop: true,
-          typeSpeed: 50, /* velocidade digitação */
-          backSpeed: 40, /* velocidade em apagar */
-          backDelay: 500, /* tempo de espera */
-      });
+    on('click', '.mobile-nav-toggle', function () {
+        select('body').classList.toggle('mobile-nav-active')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
 
-  }
+    // Efeito Digitação
+    const typed = select('.typed')
 
-  new PureCounter();
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
 
-  /* Função Waypoints */
-    // JS para Skills = Habulidades ou Vendas
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100, //velocidade digitação
+            backSpeed: 50, //velocidade em apagar
+            backDelay: 2000, //tempo de espera
+        });
 
+    }
+
+    new PureCounter();
+
+    /* Função Waypoints */
+    // JS para Skills = Habilidades ou Vendas
 
     let skilsContent = select('.skills-content');
-    if (skilsContent){ //verifica se o elemento existe
-        new Waypoint({
+    if (skilsContent) {  //verifica o elemento existe
+        new Waypoint({   //cria uma instancia
             element: skilsContent,
-            offset: '80%', //janela de visualização
-            handler: function(diretion) { //cria uma instancia
+            offset: '80%',  //janela de visualização
+            handler: function (direction) {
                 let progress = select('.progress .progress-bar', true);
                 progress.forEach((el) => {
                     el.style.width = el.getAttribute('aria-valuenow') + '%'
                 });
-                }
+            }
         })
     }
 
-
-
-
-    /* Isotope - Biblioteca para Filtro */
-    window.addEventListener( 'load', () => {
+    // Isotope - Biblioteca para Filtro
+    window.addEventListener('load', () => {
         let portfolioContainer = select(".portfolio-container");
-        if (portfolioContainer){
-            let portfolioIsotope = new Isotope(portfolioContainer,{
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
                 itemsSelector: '.portfolio-item'
             });
 
-            let portfolioFilters = select('#portfolio-filters li' , true);
+            // Selecionar todos as Listas (li) do html
+            let portfolioFilters = select('#portfolio-filters li', true);
+
+            on('click', '#portfolio-filters li', function(e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function(el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+
+
+            }, true);
         }
-
-
     });
+
 
 
 
